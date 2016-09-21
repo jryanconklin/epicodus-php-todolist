f0e69374da4dfd2bb737081b52b22a63a31d9201
J. Ryan Conklin <jryanconklin@gmail.com>
2016-09-21 09:03:32 -0700

    Update Connection to Work at Epicodus.

--- inc/ConnectionTest.php
+++ inc/ConnectionTest.php
@@ -1,17 +1,23 @@
 <?php
 
-//Home Connection
-    try {
-    $DB = new PDO("mysql:host=127.0.0.1;dbname=to_do_test;port=8889","root","root");
-    $DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (Exception $e) {
-      echo "Unable to Connect";
-      //echo $e->getMessage();
-      exit;
-    } // native exception class
+//Home Connection #1
+    // try {
+    // $DB = new PDO("mysql:host=127.0.0.1;dbname=to_do_test;port=8889","root","root");
+    // $DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // } catch (Exception $e) {
+    //   echo "Unable to Connect";
+    //   //echo $e->getMessage();
+    //   exit;
+    // } // native exception class
 
-//Epicodus Connection
-    // $server = 'mysql:host=localhost;dbname=to_do_test';
+// Home Connection #2
+    // $server = 'mysql:host=localhost:8889;dbname=to_do_test';
     // $username = 'root';
     // $password = 'root';
     // $DB = new PDO($server, $username, $password);
+
+//Epicodus Connection
+    $server = 'mysql:host=localhost;dbname=to_do_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
